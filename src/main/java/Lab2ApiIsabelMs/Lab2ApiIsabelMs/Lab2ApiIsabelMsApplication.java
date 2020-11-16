19a0ad53987f88a5ff6116b88c75d363ca8dbaee
DESKTOP-3M2FJBS\USUARIO <isabellanaja0@gmail.com>
2020-11-16 16:41:44 +0100

    commit inicio-ejercicio1

---
+++ src/main/java/Lab2ApiIsabelMs/Lab2ApiIsabelMs/Lab2ApiIsabelMsApplication.java
@@ -0,0 +1,13 @@
+package Lab2ApiIsabelMs.Lab2ApiIsabelMs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2ApiIsabelMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2ApiIsabelMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Lab2ApiIsabelMs/Lab2ApiIsabelMs/Lab2ApiIsabelMsApplicationTests.java
@@ -0,0 +1,13 @@
+package Lab2ApiIsabelMs.Lab2ApiIsabelMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2ApiIsabelMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
